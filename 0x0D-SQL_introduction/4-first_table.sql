15b3f9aa674cbd94763a3d434f28238462fa8584
Mohammed Mohammed A <abbaroze@gmail.com>
2022-11-01 07:16:48 -0700

    added table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- creating first table 
 -- should not fail and also have id and name attribute
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(25));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
