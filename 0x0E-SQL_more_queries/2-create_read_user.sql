1594fb3feb8b328a5ef864286525e084e8ad77f0
Mohammed Mohammed A <abbaroze@gmail.com>
2022-11-02 07:03:57 -0700

    granting reading access

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- creating a databse and a user under the datbase
+-- database should not fail if not available
+-- user should not fail if not available
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
